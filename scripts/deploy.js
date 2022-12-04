12142abbfcca446839505326dffbf4708c24ca9f
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-12-04 22:43:54 +0100

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,40 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  // Calling the contracts
+const Zoe = await ethers.getContractFactory("Zoe");
+
+// Deploy the contract
+const zoe = await Zoe.deploy()
+
+await zoe.deployed()
+
+
+// print the contract address
+console.log("Zoe Bank deployed to: ", zoe.address);
+
+
+
+  console.log("Sleeping.....");
+  // Wait for polygonscan to notice that the contract has been deployed
+  await sleep(10000);
+
+  // Verify the Zoe Bank  contract after deploying
+  await hre.run("verify:verify", {
+    contract: "contracts/Zoe.sol:Zoe",
+    address: accounts.address,
+    constructorArguments: [],
+  });
+
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
